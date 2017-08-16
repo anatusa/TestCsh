970f4b24415908002fedf5ce3c4b92c661d35607
Anat Usa-ampai <a.usa-ampai@togetherteam.co.th>
2017-08-16 16:33:47 +0700

    Update Home.js

--- AppRead/Home/Home.js
+++ AppRead/Home/Home.js
@@ -28,7 +28,7 @@ var serviceRequest;
         } else if (Office.context.mailbox.item.attachments.length == 0) {
             var testButton = document.getElementById("testButton");
             testButton.onclick = "";
-            showToast("No attachments", "There are no attachments on this item555.");
+            showToast("No attachments", "There are no attachments on this item666.");
         } else {
 
             // Initalize a context object for the app.
@@ -65,7 +65,7 @@ function makeServiceRequest() {
     xhr = new XMLHttpRequest();
 showToast5("makeServiceRequest", "xhr");
     // Update the URL to point to your service location.
-    xhr.open("POST", "http://webserviceatt.azurewebsites.net/publish/AttachmentService", true);
+    xhr.open("POST", "http://webserviceatt.azurewebsites.net/api/publish/AttachmentService", true);
  showToast5("makeServiceRequest", "called url ");
     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     xhr.onreadystatechange = requestReadyStateChange;
