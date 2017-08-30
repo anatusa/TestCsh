385d8a2de26806dffeb7145bed44b695a0c91a0f
Anat Usa-ampai <a.usa-ampai@togetherteam.co.th>
2017-08-30 14:05:50 +0700

    1
    1

--- AppRead/Home/ews.js
+++ AppRead/Home/ews.js
@@ -5,10 +5,12 @@ exch.Credentials = new ews.ExchangeCredentials("a.usa-ampai@togetherteam.co.th",
 //set ews endpoint url to use
 exch.Url = new ews.Uri("https://outlook.office365.com/EWS/Exchange.asmx"); //
 
-var item = ews.Item.Bind(exch,"sdfasafsadf");
+var item = ews.EmailMessage.bind(exch,new ews.ItemId("AAMkADcwM2EzYmEyLTZhNmQtNDk0ZC04NzQyLWE4YjBiNmY2M2RhMgBGAAAAAADKGV1LwqwrToX/Go0JvvSFBwAJvBImc5C0RLPrNQXCLAYlAAAAAAEMAAAJvBImc5C0RLPrNQXCLAYlAAASgWqiAAA="));
 
    
-	item.Load(new ews.PropertySet(ews.ItemSchema.MimiContent));
+	item.Load();
+	//exch.LoadPropertiesForItems(item,ews.PropertySet.FirstClassProperties);
+	console.log(item.Subject);
 	var mc = item.MimiContent;
 	var fs = new FileStream("C:\test.eml",FileMode.Create);
 	fs.Write(mc.Content,0,mc.Content.Length);
